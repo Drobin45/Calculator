995db081e53882063fbefc94e96f64d72cf1ec5c
Daniel <drobin897@gmail.com>
2017-09-22 10:18:05 -0500

    Basic print written, first of four methods begun. Need to fix error in Sum()

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,8 +6,25 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your first whole interger");
+            int one = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your operator (+, -, x, or %");
+            string oper = Console.ReadLine();
+            Console.WriteLine("Please enter your second whole interger");
+            int two = int.Parse(Console.ReadLine());
+
+            if (oper == "+")
+            {               
+                Console.WriteLine(Sum(one, two));
+            }
 
 
-        } //Method Line ----------------------------------------------------------------------------------------
+        } //Method Line ----------------------------------------------------------------------------------------  
+        public virtual int Sum(int one, int two)
+        {
+            int add = one + two;
+            return add;
+        }
     }
 }
+
